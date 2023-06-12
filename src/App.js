35d2513734de923e0ddc1481874bd4cc62e8364f
Tarun Mittal <tm6622@g.rit.edu>
2023-06-12 15:52:30 -0400

    added library of songs

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './styles/app.scss';
 
 import Player from './components/Player';
 import Song from './components/Song';
+import Library from './components/Library';
+
 import data from './utils';
 
 function App() {
@@ -19,6 +21,7 @@ function App() {
         isPlaying={isPlaying} 
         currentSong= {currentSong} 
       />
+      <Library songs={songs} />
     </div>
   );
 };
