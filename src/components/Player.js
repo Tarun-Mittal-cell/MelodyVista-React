c630e49e613d00a8400f78721f8c28a68f540aa3
Tarun Mittal <tm6622@g.rit.edu>
2023-06-12 14:49:15 -0400

    added pause feature

--- src/components/Player.js
+++ src/components/Player.js
@@ -3,10 +3,16 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlay, faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons';
 
 
-const Player = ({ currentSong }) => {
+const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
     const audioRef = useRef(null);
     const playSongHandler = () => {
-        audioRef.current.play();
+        if(isPlaying) {
+            audioRef.current.pause();
+            setIsPlaying(!isPlaying);
+        } else {
+            audioRef.current.play();
+            setIsPlaying(!isPlaying);
+        }
     };
     return (
         <div className="player">
